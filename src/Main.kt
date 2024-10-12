433cdb74efd064b4a76621aea6a89219f3dfeca8
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2024-10-12 18:12:15 +0300

    солдатский гранж

--- src/Main.kt
+++ src/Main.kt
@@ -1,44 +1,75 @@
 fun main() {
     val mockClient = Client()
-    val response = mockClient.perform(200, "OK").let {
-        it.andExpect({it.responseMatchers.status { it.statusResponseMatchers.isOk() }},
-            {it.responseMatchers.body { it.bodyResponseMatchers.isNotNull() }})
-            .andDo { response -> println(response) }
-    }.response
-    println(response.code)
+    val response = mockClient.perform(200, "OK")
+        .andExpect {
+            status {
+                isOk()
+            }
+            body {
+                isNotNull()
+            }
+        }.andDo { response ->
+            println(response)
+        }.response
 }
 
 
 class ResponseActions(val code: Int, val body: String?) {
     val response: Response = Response(code, body)
-    val responseMatchers: ResponseMatchers = ResponseMatchers()
-    val bodyResponseMatchers: BodyResponseMatchers = BodyResponseMatchers()
-    val statusResponseMatchers: StatusResponseMatchers = StatusResponseMatchers()
 
-    fun andDo(callback: (ResponseActions) -> Unit): ResponseActions {
+
+    fun andDo(callback: (Response) -> Unit): ResponseActions {
+        callback(response)
         return this
     }
-    fun andExpect(callback1: () -> Unit, callback2: () -> Unit): ResponseActions {
+    fun andExpect(callback: ResponseMatchers.() -> Unit): ResponseActions {
+        callback(ResponseMatchers())
         return this
     }
 
-    class ResponseMatchers() {
-        fun status(callback: () -> Unit) {
-            TODO()
+    inner class ResponseMatchers() {
+
+        fun body(callback: BodyResponseMatchers.() -> Unit) {
+            callback(BodyResponseMatchers())
         }
-        fun body(callback: () -> Unit) {
-            TODO()
+
+        fun status(callback: StatusResponseMatchers.() -> Unit) {
+            callback(StatusResponseMatchers())
         }
     }
 
-    class BodyResponseMatchers() {
-        fun isNull() {}
-        fun isNotNull() {}
+    inner class BodyResponseMatchers() {
+        fun isNull() {
+            if (!body.isNullOrBlank())
+                throw BodyResponseMatchersException()
+        }
+        fun isNotNull() {
+            if (body.isNullOrBlank())
+                throw BodyResponseMatchersException()
+        }
     }
 
-    class StatusResponseMatchers() {
-        fun isOk() {} // если статус не 200, то выбросить исключение
-        fun isBadRequest() {} // если статус не 400, то выбросить исключение
-        fun isInternalServerError() {} // если статус не 500, то выбросить исключение
+    inner class StatusResponseMatchers() {
+        fun isOk() {
+            if (code != 200)
+                throw StatusResponseMatchersException()
+        }
+        fun isBadRequest() {
+            if (code != 400)
+                throw StatusResponseMatchersException()
+        }
+        fun isInternalServerError() {
+            if (code != 500)
+                throw StatusResponseMatchersException()
+        }
     }
 }
+
+
+sealed class ResponseMatchersException(message:String): Exception(message)
+
+
+class StatusResponseMatchersException(): ResponseMatchersException("Ошибка запроса")
+
+
+class BodyResponseMatchersException(): ResponseMatchersException("Ошибка тела запроса")
